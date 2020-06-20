094380bd02fc1dccf053c81d1d0637e153d5cb95
Anderson Pereira <andy2903.alp@gmail.com>
2020-06-19 23:05:23 -0300

    init cronJob gtm

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ app = express();
 
 cron.schedule("* * * * * *", () => console.log("Executando a tarefa a cada 1 minuto"));
 
+const CronJob = require('cron').CronJob
+const job = new CronJob('* * * * * *', () => {
+console.log('tarefa agendada')
+}, null, true, 'America/Sao_Paulo')
+
 app.listen(1313);
